a2209b1527b1711d4a5f7fbd582afaf7a806d1ec
sabta <sabtaamendes@gmail.com>
2022-06-22 12:15:00 -0400

    primeiros passos projeto - css, html, js

---
+++ settings.js
@@ -0,0 +1,23 @@
+let batepapo = [{
+    
+}]
+
+
+
+
+//fazer função para pegar a lista no servidor
+function buscarDados(){
+
+    const promessa = axios.get('http://mock-api.driven.com.br/api/v6/uol');
+    promessa = resposta.then(renderizarDados);
+
+//variável .get
+//variável .then
+}
+
+//função pra renderizar o arquivo
+function renderizarDados(resposta){
+
+    //selecionar a classes a ser modificada
+    // exibir a resosta
+}
