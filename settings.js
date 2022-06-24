dcb6be44a51562f2a3b556666324689187cc76bc
sabta <sabtaamendes@gmail.com>
2022-06-24 13:28:35 -0400

    status usuário - js

--- settings.js
+++ settings.js
@@ -1,36 +1,59 @@
-// perguntar o nome
+// entrar na sala
+let cadastro = prompt("Qual é o seu lindo nome?");
 
+cadastrarUsuario();
 function cadastrarUsuario() {
-  
-    const cadastro = prompt("Qual é o seu lindo nome?");
+    
     const pergunta = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",
         {
             name: cadastro
         }
     );
-    pergunta.then(AcessoLiberado);
-    pergunta.catch(nomeEnviadoComErro);
-}
-cadastrarUsuario();
-
-function AcessoLiberado() {
-    alert("Acesso liberado!");
+    pergunta.then(acessoLiberado);
+    pergunta.catch(acessoNegado);
 }
 
+function acessoLiberado(response) {
+   
+    if(response.status === 200) {
+        alert("Acesso Liberado!");
+    }
+    // mensagem entrou na sala
+    const li = document.querySelector(".mensagem-status");
+    li.innerHTML = `${cadastro} entra na sala... `;
+    conectadosStatus();
+}
 
-function nomeEnviadoComErro(erro) {
-    console.log(erro)
+function acessoNegado(erro) {
+   
     if (erro.response.status === 400) {
         prompt("Cadastre um novo nome!");
     }
-    if (erro.response.status === 200) {
-        alert("Nome cadastrado com sucesso!");
-    }
 }
 
-function buscarDados() {
-    const buscar = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+//manter conexão
 
-    buscar.then(AcessoLiberado);
+function conectadosStatus() {
+    
+    if(cadastro.status !== undefined) {
+        conectado = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",
+            {
+                name: cadastro
+            }
+        )
+        setInterval(conectado, 5000)
+        conectado.then(mensagemStatus);
+    }return //mensagem status
 }
 
+function mensagemStatus() {
+    
+    const msgStatus = document.querySelector(".mensagem-status")
+    msgStatus.innerHTML = `${cadastro} saiu da sala... `;
+    const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    promisse.then(acessoLiberado);
+
+}
+function mensagemErrro() {
+
+}
