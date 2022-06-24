e62bd4048c2328d3b7fc4c2ff85e1ae8b0747eb3
sabta <sabtaamendes@gmail.com>
2022-06-24 15:02:31 -0400

    função reajustada informarConexao

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 // entrar na sala
 let cadastro = prompt("Qual é o seu lindo nome?");
-
+let promisse;
 cadastrarUsuario();
 function cadastrarUsuario() {
-    
+
     const pergunta = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",
         {
             name: cadastro
@@ -14,46 +14,46 @@ function cadastrarUsuario() {
 }
 
 function acessoLiberado(response) {
-   
-    if(response.status === 200) {
+
+    if (response.status === 200) {
         alert("Acesso Liberado!");
     }
     // mensagem entrou na sala
     const li = document.querySelector(".mensagem-status");
     li.innerHTML = `${cadastro} entra na sala... `;
-    conectadosStatus();
 }
 
 function acessoNegado(erro) {
-   
+
     if (erro.response.status === 400) {
         prompt("Cadastre um novo nome!");
     }
 }
 
 //manter conexão
-
-function conectadosStatus() {
-    
-    if(cadastro.status !== undefined) {
-        conectado = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",
-            {
-                name: cadastro
-            }
-        )
-        setInterval(conectado, 5000)
-        conectado.then(mensagemStatus);
-    }return //mensagem status
+informarConexao();
+function informarConexao() {
+console.log(informarConexao)
+    promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
+        name: cadastro
+    });
+    promisse.then(simConectado);
+    promisse.catch(naoConectado);
 }
 
-function mensagemStatus() {
-    
-    const msgStatus = document.querySelector(".mensagem-status")
-    msgStatus.innerHTML = `${cadastro} saiu da sala... `;
-    const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-    promisse.then(acessoLiberado);
-
+function simConectado(){
+if(type.message !== undefined){
+     promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
+        type: message
+    });
+    setInterval(simConectado, 5000)
 }
-function mensagemErrro() {
+}
 
+function naoConectado(erro){
+    if(erro.type === undefined){
+        const li = document.querySelector(".mensagem-status");
+        li.innerHTML = `${cadastro} saiu na sala... `;
+    }
+clearInterval(simConectado)
 }
