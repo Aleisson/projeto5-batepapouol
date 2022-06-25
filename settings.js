1b4b0d90e35d9bd2e1de93718d109c3f3836fd4f
sabta <sabtaamendes@gmail.com>
2022-06-24 20:17:41 -0400

    função buscarMensagem

--- settings.js
+++ settings.js
@@ -34,7 +34,7 @@ function acessoNegado(erro) {
 //manter conexão
 informarConexao();
 function informarConexao() {
-console.log(informarConexao)
+    console.log(informarConexao)
     promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
         name: cadastro
     });
@@ -43,48 +43,73 @@ console.log(informarConexao)
 }
 
 
-function simConectado(type){
-if(type.message !== undefined){
-     promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
-        type: message
-    });
-    setInterval(simConectado, 5000)
-}
+function simConectado(type) {
+    if (type.message !== undefined) {
+        promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
+            type: message
+        });
+        setInterval(simConectado, 5000)
+    }
 }
 
-function naoConectado(erro){
-    if(erro.type === undefined){
+function naoConectado(erro) {
+    if (erro.type === undefined) {
         const li = document.querySelector(".mensagem-status");
         li.innerHTML = `${cadastro} saiu na sala... `;
     }
-clearInterval(simConectado)
+    clearInterval(simConectado)
 }
 
 //buscarMensagens
 
-function buscarMensagem(){
+function buscarMensagem() {
     const buscar = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     buscar.then(executarResposta);
     console.log(buscar)
 }
-function executarResposta(response){
-    console.log(executarResposta)
+function executarResposta(response) {
     lista = response.data;
 }
 
-function renderizarMensagem(){
-    const mensagemStatus = document.querySelector(".mensagem-status");
-    const mensagemReservada = document.querySelector(".mensagem-reservada");
-    const mensagemParaTodos = document.querySelector(".mensagem-normal");
+function renderizarMensagemParaTodos() {
+    const paraTodos = document.querySelector(".mensagem-normal");
+    paraTodos.scrollIntoView();
+    paraTodos.innerHTML = "";
 
-for(let i = 0; i > lista.length; i++){
-   
-   <div class="mensagem">
-            <ul>
-                <li class="mensagem-status" onclick="(this)"></li>
-                <li class="mensagem-reservada"></li>
-                <li class="mensagem-normal"></li>
-            </ul>
-        </div>
+    for (let i = 0; i > lista.length; i++) {
+        paraTodos.inneHTML += `
+        <li class="mensagem-normal">
+        ${lista[i].type}
+        </li>
+`
+    }
 }
+
+function renderizarMensagemStatus() {
+    const status = document.querySelector(".mensagem-status");
+    status.scrollIntoView();
+    status = "";
+    for (let i = 0; i > lista.length; i++) {
+        status.inneHTML += `
+        
+        <li class="mensagem-status">
+        ${lista[i].type}
+        </li>
+         `
+    }
+}
+function renderizarMensagemReservada() {
+    const reservada = document.querySelector(".mensagem-reservada");
+    reservada.scrollIntoView();
+    reservada.innerHTML = "";
+    for (let i = 0; i < lista.length; i++) { //maior ou menor?
+        resevarda += `
+        <li class="mensagem-reservada">
+            ${lista[i].type}
+        </li>
+   ` }
+}
+
+function envioDeMensagem(){
+
 }
