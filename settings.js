e2a87ecd5d3b6f55df25c5e3ebdbaec4f62c456f
sabta <sabtaamendes@gmail.com>
2022-06-26 21:06:18 -0400

    JS

--- settings.js
+++ settings.js
@@ -1,145 +1,98 @@
-// entrar na sala
-let cadastro;
-let promisse;
+let nome;
 let lista = [];
 let paraTodos;
 let onlineSim;
 let message;
-let ultimaMenssagem;
+
+// entrar na sala
 cadastrarUsuario();
 function cadastrarUsuario() {
-    cadastro = prompt("Qual é o seu lindo nome?");
-    const pergunta = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",
-        {
-            name: cadastro
-        }
-    );
+    nome = prompt("Qual é o seu lindo nome?");
 
-    pergunta.then(acessoLiberado);
-    pergunta.catch(acessoNegado);
-    setInterval(envioDeMensagem, 3000)
+    usuario = {
+        name: nome
+    }
+
+    const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
+    setInterval(informarConexao, 5000, nome)
+    promisse.then(acessoLiberado);
+    promisse.catch(acessoNegado);
+    console.log(promisse)
 }
 
 function acessoLiberado() {
     alert("Acesso Liberado!");
-    buscarMensagem();
+    // buscarMensagem();
+    console.log("SIM")
 }
 
-function acessoNegado() {
-    alert("Cadastre um novo nome! Esse já existe!")
+function acessoNegado(erro) {
+    if (erro.response === 400) {
+        alert("Cadastre um novo nome! Esse já existe!");
+    }
     cadastrarUsuario();
+    console.log("NÃO")
 }
+
 //manter conexão
-
-informarConexao();
 function informarConexao() {
-    promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
-        name: cadastro
-    });
-    promisse.then(simConectado);
-    promisse.catch(naoConectado);
+    console.log("CONEXAO")
 }
-
-function simConectado(type) {
-    if (type.message !== undefined) {
-        promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
-            type: message
-        });
-        promisse.then(envioDeMensagem);
-    }
-}
-
-function naoConectado(erro) {
-    if (erro.type === undefined) {
-        const li = document.querySelector(".mensagem-status");
-        li.innerHTML = `${cadastro}`;
-    }
-    clearInterval(simConectado);
-}
+informarConexao();
 
 //buscarMensagens
-function mensagemScroll() {
-    const elementosAparecer = document.querySelector('.enviarMensagem').value;
-    elementosAparecer.scrollIntoView();
-
-}
-
-function reloadThePage() {
-    window.location.reload(".enviarMensagem").value;
-}
-
 function buscarMensagem() {
     const buscar = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     buscar.then(executarResposta);
-
+    setInterval(buscarMensagem, 3000)
+    console.log("buscar")
 }
-
 function executarResposta(response) {
     lista = response.data;
-    renderizarTodasMensagens();
+    console.log("response")
+    renderizarMensagemParaTodos();
 }
 
-function renderizarTodasMensagens() {
 
-renderizarMensagemStatus();
-renderizarMensagemParaTodos();
-}
+function renderizarMensagem() {
 
-function renderizarMensagemParaTodos() {
-    paraTodos = document.querySelector(".mensagem-normal");
-    paraTodos.innerHTML = "";
+    let exibirMensagem = document.querySelector(".mensagem"); //main
     for (let i = 0; i > lista.length; i++) {
         if (lista[i].type === "message") {
-            paraTodos.inneHTML += `
+            exibirMensagem.inneHTML += `
             <li class="mensagem-normal">
             <p><span class="hora">${lista[i].time}</span> <span class ="usuario">${lista[i].from}</span><span class="status">${lista[i].message}</span></p>
         </li>
          `
         }
-    }
-    ultimaMenssagem = 
-    envioDeMensagem();
-}
-
-function renderizarMensagemStatus() {
-    onlineSim = document.querySelector(".mensagem-status");
-    onlineSim.innerHTML = "";
-    for (let i = 0; i > lista.length; i++) {
         if (lista[i].type === "status") {
-            onlineSim.inneHTML += `
+            exibirMensagem.inneHTML += `
             <li class="mensagem-status">
             <p><span class="hora">${lista[i].time}</span> <span class ="usuario">${lista[i].from}</span><span class="status">${lista[i].message}</span></p>
         </li>
          `
         }
     }
-
-    envioDeMensagem();
+    let ultimaMensagem = exibirMensagem.lastChild;
+    ultimaMensagem.scrollIntoView();
+    console.log(ultimaMensagem)
+    digitarMensagem();
 }
 
-//enviar mensagem para o servidor  AJEITAR
-function envioDeMensagem() {
-    const usuario = document.querySelector(".mensagem");
-    const text = document.querySelector(".digitarMensagem").value;
-     message = document.querySelector(".mensagem-status .mensagem-normal");
-    const todos = document.querySelector(".mesagem-normal");
+function exibirMensagem() {
+    let escreverMensagem;
 
-    const enviarMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",
-        {
-            from: usuario,
-            to: todos,
-            text: text,
-            type: message
-        }
-    );
-    enviarMensagem.then(renderizarMensagemParaTodos);
-    console.log(enviarMensagem)
-}
+    const texto = document.querySelector(".rodape").value;
+    escreverMensagem = {
+        from: nome,
+        to: "Todos",
+        text: texto,
+        type: "message"
 
-function digitarMensagem(input) {
-    if (input.value === message) {
-        let acionarIcone = document.querySelector(".icone")
-        acionarIcone.classList.add("mensagem")
     }
+    const enviarMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", escreverMensagem);
+
+    enviarMensagem.then();
+    enviarMensagem.catch();
+    document.querySelector("input").value = "";
 }
-digitarMensagem();
